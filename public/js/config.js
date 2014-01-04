2fe6a7238a18c41db4850fbc661589d0dec3ddc7
Gaston Avila <avila.gas@gmail.com>
2014-01-03 21:32:03 -0300

    added bootstrap javascript files and shim config

--- public/js/config.js
+++ public/js/config.js
@@ -6,11 +6,16 @@ require.config({
         },
         handlebars: {
             exports: 'Handlebars'
+        },
+        bootstrap: {
+            deps: ["jquery"],
+            exports: "$.fn.popover"
         }
     },
     paths: {
         "backbone": "../../bower_components/backbone-amd/backbone",
         "jquery": "../../bower_components/jquery/jquery",
+        "bootstrap": "../../bower_components/bootstrap/docs/assets/js/bootstrap",
         "OpenLayers": "lib/OpenLayers",
         // "OpenLayers": "../../bower_components/openlayers/lib/OpenLayers",
         "underscore": "../../bower_components/underscore-amd/underscore",
