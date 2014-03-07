dd9efa6ea34096e5fcd39d854d51b6c73ccefaab
Gaston Avila <avila.gas@gmail.com>
2014-03-06 23:59:27 -0300

    kml and kmz exporting for stops

--- server/xml.py
+++ server/xml.py
@@ -25,31 +25,43 @@ import gtfsdb
 import pystache
 import codecs
 import config
+import zipfile
 
 class Kml(object):
     """docstring for Kml"""
-    def __init__(self):
+    def __init__(self, template):
         super(Kml, self).__init__()
+        self.renderer = pystache.Renderer()
+        self.templateFile = codecs.open(template, 'r', 'utf-8')
+        self.template = pystache.parse(self.templateFile.read())
 
-    def renderStops(self, stops):
-        renderer = pystache.Renderer()
-        with codecs.open('server/templates/stops.mustache', 'r', 'utf-8') as templateFile:
-                template = templateFile.read()
-                parsed = pystache.parse(template)
+    def renderKml(self, data, filename):
+        kmlString = self.renderer.render(self.template, data)
 
-        return renderer.render(parsed, {'stops': stops})
+        with codecs.open(filename, 'w', 'utf-8') as outFile:
+            outFile.write(kmlString)
+
+    def renderKmz(self, data, filename):
+        kmlString = self.renderer.render(self.template, data)
+
+        with zipfile.ZipFile(filename, mode="w", compression=zipfile.ZIP_DEFLATED) as kmz:
+            kmz.writestr('doc.kml', kmlString.encode('utf-8'))
 
 def main():
     db = o.dbInterface(config.DATABASE)
     toolbox = gtfsdb.toolbox(db)
 
-    stops = toolbox.stops()
-    kml = Kml()
-    kmlString = kml.renderStops(stops)
+    trips = {trip['trip_id']:dict(trip) for trip in db.select('trips')}
 
-    outFilename = 'compiled/stops.kml'
-    with codecs.open(outFilename, 'w', 'utf-8') as outFile:
-        outFile.write(kmlString)
+    stops = toolbox.stops()
+    for stop in stops:
+        stop_id = stop['stop_id']
+        l = db.select('stop_seq',stop_id=str(stop_id))
+        stop['trips'] = [trips[t['trip_id']] for t in l]
+
+    kml = Kml('server/templates/stops.mustache')
+    kml.renderKml({'stops': stops}, 'public/kml/stops.kml')
+    kml.renderKmz({'stops': stops}, 'public/kml/stops.kmz')
 
 if __name__ == '__main__':
     main()
