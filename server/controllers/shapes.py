37b904c33f2780e042c5807f807bae34e63d65a0
Gaston Avila <avila.gas@gmail.com>
2014-06-23 18:31:36 -0300

    shape save controller done

--- server/controllers/shapes.py
+++ server/controllers/shapes.py
@@ -19,5 +19,18 @@ def shape(db, shape_id):
 @app.put('/api/shape/<shape_id>')
 def shape(db, shape_id):
   geojsonShape = request.json
-  print geojsonShape
-  # return Shape.set(shape_id, geojsonShape)
+  for feature in geojsonShape['features']:
+    if feature['geometry']['type'] == 'LineString':
+      coordList = feature['geometry']['coordinates']
+      db.query(Shape).filter_by(shape_id = shape_id).delete()
+      for i,pt in enumerate(coordList):
+        d = {
+          'shape_id': shape_id,
+          'shape_pt_lon': pt[0],
+          'shape_pt_lat': pt[1],
+          'shape_pt_sequence': i+1
+          }
+        shape_pt = Shape(**d)
+        db.add(shape_pt)
+
+  return {'success': True,'shape_id': shape_id}
