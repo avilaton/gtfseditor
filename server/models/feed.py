4d56979cdd623d31e44dd908f1b1d66ef4602c5c
Gastón <avila.gas@gmail.com>
2014-07-21 17:50:38 -0300

    40% on feed factory

--- server/models/feed.py
+++ server/models/feed.py
@@ -11,6 +11,10 @@ from server import engine
 from server.models import Route
 from server.models import Agency
 from server.models import Trip
+from server.models import Calendar
+from server.models import CalendarDate
+from server.models import Stop
+from server.models import StopSeq
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
@@ -19,7 +23,7 @@ Session = sessionmaker(bind=engine)
 db = scoped_session(Session)
 
 class Feed(object):
-  """docstring for Feed"""
+  """GTFS schedule feed factory"""
   def __init__(self, arg):
     self.arg = arg
     self.schedule = transitfeed.Schedule()
@@ -28,7 +32,14 @@ class Feed(object):
     return 'a feed' 
 
   def build(self):
+    logger.info("Feed build started")
     self.loadAgencies()
+    self.loadCalendar()
+    self.loadCalendarDates()
+    self.loadRoutes()
+    self.loadTrips()
+    # self.loadStops()
+
     self.schedule.WriteGoogleTransitFeed('tmp/test.zip')
 
   def loadAgencies(self):
@@ -39,4 +50,78 @@ class Feed(object):
           row.agency_timezone, agency_id=row.agency_id)
       agency.agency_phone = row.agency_phone
       agency.agency_lang = row.agency_lang
-    self.schedule.SetDefaultAgency(self.schedule.GetAgencyList()[0])
+    if len(self.schedule.GetAgencyList()):
+      self.schedule.SetDefaultAgency(self.schedule.GetAgencyList()[0])
+
+  def loadCalendar(self):   
+    logger.info("Loading Calendar")
+    for s in db.query(Calendar).all():
+      service = transitfeed.ServicePeriod()
+      service.SetServiceId(s.service_id)
+      service.SetStartDate(str(s.start_date))
+      service.SetEndDate(str(s.end_date))
+      service.SetDayOfWeekHasService(0, bool(s.monday) )
+      service.SetDayOfWeekHasService(1, bool(s.tuesday) )
+      service.SetDayOfWeekHasService(2, bool(s.wednesday) )
+      service.SetDayOfWeekHasService(3, bool(s.thursday) )
+      service.SetDayOfWeekHasService(4, bool(s.friday) )
+      service.SetDayOfWeekHasService(5, bool(s.saturday) )
+      service.SetDayOfWeekHasService(6, bool(s.sunday) )
+      self.schedule.AddServicePeriodObject(service)
+
+  def loadCalendarDates(self):
+    """Inserts calendar date exceptions from calendar_dates table"""
+    logger.info("Loading Calendar Dates")
+    
+    exceptions = {"1": True, "2": False}
+
+    for date in db.query(CalendarDate).all():
+      service_period = self.schedule.GetServicePeriod(date.service_id)
+      service_period.SetDateHasService(date.date, has_service=exceptions[date.exception_type])
+
+  def loadRoutes(self):
+    """Loads active routes into schedule"""
+    logger.info("Loading Routes")
+
+    for route in db.query(Route).all():
+      route_id = route.route_id
+      # if str(route.active) is not "TRUE":
+      #   continue
+      logger.info("Loading route_id: {0}".format(route_id))
+      r = self.schedule.AddRoute(short_name=route.route_short_name, 
+          #long_name=route.route_long_name, 
+          long_name='', 
+          route_id=route.route_id,
+          route_type=route.route_type)
+      r.agency_id = route.agency_id
+      r.route_color = route.route_color
+      r.route_text_color = route.route_text_color
+
+  def loadTrips(self):
+    """Loads active trips into schedule"""
+    logger.info("Loading Trips")
+
+    for route in self.schedule.GetRouteList():
+      route_id = route['route_id']
+      for t in db.query(Trip).filter_by(route_id=route_id).all():
+        for service in self.schedule.GetServicePeriodList():
+          trip_id = t.trip_id + '.' + service.service_id
+          trip = route.AddTrip(trip_id = trip_id, headsign=t.trip_headsign)
+          trip.service_id = service.service_id
+          trip.shape_id = t.trip_id
+          trip.direction_id = t.direction_id
+          logger.info("Loading trip_id: {0}".format(trip_id))
+
+  def loadStops(self):
+    logger.info("Loading Stops")
+    # query = db.query(Stop, StopSeq).join(StopSeq, Stop.stop_id == StopSeq.stop_id).all()
+    # query = db.query(Stop).all()
+    query = db.query(Stop)
+
+    print query
+    for stop in query:
+        lat = stop.stop_lat
+        lng = stop.stop_lon
+        stop_id = stop.stop_id
+        stop = self.schedule.AddStop(lat=float(lat),lng=float(lng),name=s['stop_name'],stop_id=stop_id)
+        stop.stop_code = s['stop_id']
