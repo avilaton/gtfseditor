d727c815b3105eebaa1a7b87936b5e709ed99f05
Gastón <avila.gas@gmail>
2014-07-25 14:50:55 -0300

    clean database dir

--- server/config.py
+++ server/config.py
@@ -3,7 +3,7 @@
 
 import os
 
-DATABASE_URL = 'sqlite:///database/dev.sqlite'
+DATABASE_URL = 'sqlite:///dev.sqlite'
 # DATABASE_URL = 'sqlite:///database/test.sqlite'
 # DATABASE_URL = 'sqlite:///database/1.0.9.sqlite'
 # DATABASE_URL = 'sqlite:///:memory:'
