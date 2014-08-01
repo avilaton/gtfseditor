d03a47ace3ad55db36734ba858079c46f3bb9b76
Gaston Avila <avila.gas@gmail.com>
2014-08-01 17:21:14 -0300

    db addres

--- server/config.py
+++ server/config.py
@@ -5,12 +5,12 @@ import os
 
 DEBUG = False
 
-DATABASE_URL = 'sqlite:///dev.sqlite'
+# DATABASE_URL = 'sqlite:///dev.sqlite'
 # DATABASE_URL = 'sqlite:///database/test.sqlite'
 # DATABASE_URL = 'sqlite:///../cba.sqlite'
 # DATABASE_URL = 'sqlite:///:memory:'
 # DATABASE_URL = 'postgres:///mza'
 # heroku setup
-# DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres:///mza')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres:///mza')
 
 BUILD_MODE = 'cba'
