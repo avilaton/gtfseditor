deac7b042118a20ca6678e881de830f9f8dcfc49
Gaston Avila <avila.gas@gmail.com>
2014-09-01 23:45:44 -0300

    clean import manage task

--- manage.py
+++ manage.py
@@ -6,15 +6,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 from server import engine
-
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 
 Session = sessionmaker(bind=engine)
 db = scoped_session(Session)
 
-from server.models import StopSeq
-from server.models import Shape
+from server.models import *
 
 from server.collections.interpolation import Interpolator
 from server.collections.populator import StopTimesFactory
@@ -24,24 +22,20 @@ import os
 import glob
 import zipfile
 import optparse
-
-from server.models import Feed
+import csv
+import codecs
 
 TMP_FOLDER = 'tmp/'
 
 def create_all():
+  logger.info("Creating all tables")
   from server.models import Base
   Base.metadata.create_all(engine)
 
 def drop_all():
+  logger.info("Droping all tables")
   from server.models import Base
   Base.metadata.drop_all(engine)
-  # db.query(Stop).delete()
-  # db.query(Shape).delete()
-  # db.query(StopSeq).delete()
-  # db.query(Route).delete()
-  # db.query(Trip).delete()
-  # db.commit()  
 
 def extract(filename, dest):
   """extract for debuging"""
@@ -117,18 +111,52 @@ def generateStopSeq():
   db.commit()
   logger.info("Time elapsed %s",time.time()-t0)
 
+def readCsv(session, engine, Model, filename, mode=None):
+  logger.info("Importing " + Model.__tablename__ + " from : %s", filename)
+  with open(filename, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    if not mode:
+      keyMap = {field:field for field in reader.fieldnames}
+      for row in reader:
+        d = {v:codecs.decode(row[k],'utf8') for k, v in keyMap.items()}
+        model = Model(**d)
+        session.merge(model)
+      session.commit()
+    elif mode in ['direct']:
+      engine.execute(Model.__table__.delete())
+      insertStmt = Model.__table__.insert()
+      engine.execute(insertStmt, [row for row in reader])
+
+def import_csv():
+  Session = sessionmaker(bind=engine)
+  session = scoped_session(Session)
+  FOLDER = 'tmp/incoming/'
+
+  readCsv(session, engine, Agency, FOLDER + 'agency.csv')
+  readCsv(session, engine, FeedInfo, FOLDER + 'feed_info.csv')
+  readCsv(session, engine, Calendar, FOLDER + 'calendar.csv')
+  readCsv(session, engine, CalendarDate, FOLDER + 'calendar_dates.csv')
+  readCsv(session, engine, Stop, FOLDER + 'stops.csv')
+  readCsv(session, engine, Route, FOLDER + 'routes.csv')
+  readCsv(session, engine, Trip, FOLDER + 'trips.csv')
+  readCsv(session, engine, TripStartTime, FOLDER + 'trips_start_times.csv')
+  readCsv(session, engine, Shape, FOLDER + 'shapes.csv', mode='direct')
+  readCsv(session, engine, StopSeq, FOLDER + 'stop_times.csv', mode='direct')
+
 if __name__ == '__main__':
   usage = """usage: %prog [options] command
 where command can be one of:
 
-  create-all        Initialize Database engine issuing CREATE statements for
-                    each table
+  create-all        Create all tables in DB
+  drop-all          Drop all tables in DB
+  import-csv        Read Csv files into DB
   build             create GTFS feed
   interpolate       interpolate trip times
   pop-times         generates stop times from stop sequences
   sort-trip         sort trip stops along shape
   gen-stop-seq      Generate stop_sequence values from stop_time
-  gen-shape-pt-seq  Generate shape point sequence from shape_pt_time"""
+  gen-shape-pt-seq  Generate shape point sequence from shape_pt_time
+  """
 
 
   parser = optparse.OptionParser(usage=usage)
@@ -162,6 +190,8 @@ where command can be one of:
     create_all()
   elif command == 'drop-all':
     drop_all()
+  elif command == 'import':
+    import_csv()
   elif command == 'interpolate':
     generate_interpolated_stop_times()
   elif command == 'pop-times':
@@ -175,6 +205,9 @@ where command can be one of:
   elif command == 'gen-shape-pt-seq':
     generateShapePtSequence()
   elif command == 'mza':
+    drop_all()
+    create_all()
+    import_csv()
     generateStopSeq()
     generateShapePtSequence()
     generate_stop_times_from_stop_seqs()
