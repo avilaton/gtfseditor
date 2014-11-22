1cd5421000abedec992baa95a9757ce7922aab23
Gaston Avila <avila.gas@gmail.com>
2014-11-22 19:45:11 -0300

    update config vars

--- server/config.py
+++ server/config.py
@@ -3,16 +3,20 @@
 
 import os
 
-DEBUG = False
+ENV = os.environ.get('ENV', 'dev')
+MODES = ["frequency", "initial-times", "full-spec"]
 
-DATABASE_URL = 'sqlite:///dev.sqlite'
-# DATABASE_URL = 'sqlite:///database/test.sqlite'
-# DATABASE_URL = 'sqlite:///../cba.sqlite'
-# DATABASE_URL = 'sqlite:///:memory:'
-# DATABASE_URL = 'postgres:///mza'
-# heroku setup
-# DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres:///mza')
+if ENV == "dev":
+	DEBUG = True
+	DATABASE_URL = 'sqlite:///dev.sqlite'
 
-# BUILD_MODE = 'frequency'
-BUILD_MODE = 'initial-times'
-# BUILD_MODE = 'full-spec'
+if ENV == "staging":
+	DEBUG = True
+	DATABASE_URL = 'postgres:///mza'
+
+if ENV == "prod":
+	DEBUG = False
+	DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres:///mza')
+
+# To be moved into a column in the routes table
+BUILD_MODE = MODES[1]
