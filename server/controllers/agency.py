01cf608d63ed0abc8f4becf753bdde11e26df0c6
marianodo <mariano@mariano-linux>
2014-12-12 19:27:07 -0300

    Get agency added but not working

--- server/controllers/agency.py
+++ server/controllers/agency.py
@@ -12,6 +12,14 @@ def agency(db):
   agencys = db.query(Agency).order_by(Agency.agency_name).all()
   return {'agency': [agency.as_dict for agency in agencys]}
 
+@app.route('/api/agency/<agency_id>')
+def getAgency(db, agency_id):
+  print "Entre"
+  agency = db.query(Agency).filter(Agency.agency_id == agency_id).first()
+  if agency:
+    return agency.as_dict
+  else:
+    abort(404, 'no such agency_id')
 
 @app.route('/api/agency/<agency_id>', method=['OPTIONS', 'PUT'])
 def updateAgency(db, agency_id):
@@ -23,15 +31,13 @@ def updateAgency(db, agency_id):
 @app.route('/api/agency', method=['OPTIONS', 'POST'])
 def createAgency(db):
   data = request.json
-  print "###",data
   agency = Agency(**data)
   db.add(agency)
   return agency.as_dict
 
 @app.route('/api/agency/<agency_id>', method=['OPTIONS', 'DELETE'])
 def deleteAgency(db, agency_id):
-  print "####",agency_id
   agency = db.query(Agency).filter(Agency.agency_id == agency_id).one()
-  print "### LO traje"
   db.delete(agency)
   return {'success': True}
+
