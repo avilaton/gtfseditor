58222b64c9d6a6fe0ea86a5ba0722961777c906d
Gaston Avila <avila.gas@gmail.com>
2015-01-07 00:45:06 -0300

    default url for api is local

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,7 @@
 define(function () {
     return {
-      server: 'http://gtfseditor.herokuapp.com/'
+      // server: 'http://gtfseditor.herokuapp.com/'
       // server: 'http://localhost:8000/'
+      server: '/'
     };
   });
