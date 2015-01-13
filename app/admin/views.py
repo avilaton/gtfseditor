ba77f88ba9b404c843ba3e450949fd26d12a2343
Mariano <mardom4164@gmail.com>
2015-01-13 11:20:52 -0300

    change to static folder in blueprint admin

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify, request, g, abort, url_for, current_app,render
 from . import admin
 
 
-app = Flask(__name__, static_folder ='/home/mariano/gaston/gtfseditor/client/')
+#app = Flask(__name__, static_folder ='/home/mariano/gaston/gtfseditor/client/')
 
 @admin.route('/index')
 def root():
-	return send_from_directory(app.static_folder, 'index.html')
+	return send_from_directory(admin.static_folder, 'index.html')
 
