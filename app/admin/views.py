ad4d67552e5f3011bd1fe0fdbfd3352a6fa45e5c
Gaston Avila <avila.gas@gmail.com>
2015-01-14 21:31:56 -0300

    use static url path and add symlink

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify, request, g, abort, url_for, current_app,render_template, send_from_directory
+from flask import send_from_directory
 from . import admin
 
-
-@admin.route('/index')
+@admin.route('/')
 def root():
 	return send_from_directory(admin.static_folder, 'index.html')
 
