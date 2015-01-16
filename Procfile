75dadb1bc3e31da1cd31e3ede8112828dd1b90c4
Gaston Avila <avila.gas@gmail.com>
2015-01-16 14:31:36 -0300

    procfile and ssl disabled

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:application -w 3
+web: gunicorn manage:app -w 3
--- app/__init__.py
+++ app/__init__.py
@@ -16,9 +16,9 @@ def create_app(config_name):
     app.config['CORS_HEADERS'] = 'X-Requested-With, Content-Type'
     cors.init_app(app)
 
-    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-        from flask.ext.sslify import SSLify
-        sslify = SSLify(app)
+    # if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+    #     from flask.ext.sslify import SSLify
+    #     sslify = SSLify(app)
 
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
