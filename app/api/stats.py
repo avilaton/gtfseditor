51b3f4c42783ed433719edc281178a8a647119a3
Gaston Avila <avila.gas@gmail.com>
2015-01-23 14:07:14 -0300

    minor fix

--- app/api/stats.py
+++ app/api/stats.py
@@ -5,7 +5,7 @@ from flask import jsonify, request, g, abort, url_for, current_app
 from .. import db
 from ..models import Stop
 from . import api
-from sqlalchemy import func 
+from sqlalchemy import func
 
 
 @api.route('/stats')
@@ -13,5 +13,5 @@ def get_stats():
 	result = db.session.query(func.min(Stop.stop_lat) \
 		,func.min(Stop.stop_lon),func.max(Stop.stop_lat),func.max(Stop.stop_lon),func.count()).first()
 	data = {'minLat': result[0], 'minLon': result[1], 'maxLat':result[2],'maxLon' :result[3],'numbers':result[4]}
-    
-	return jsonify(stops=data)
+
+	return jsonify({"stops": data})
