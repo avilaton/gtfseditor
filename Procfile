43de4c5866d8e3d10f908a9160844184719ba596
Gaston Avila <avila.gas@gmail.com>
2015-01-25 20:38:07 -0800

    drafting celery support for feed building

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn manage:app -w 3
+worker: celery -A app.tasks worker --loglevel=info
--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cors import CORS
 from config import config
 from flask.ext.login import LoginManager
 from flask.ext.bootstrap import Bootstrap
+from celery import Celery
+
 db = SQLAlchemy()
 cors = CORS()
 bootstrap = Bootstrap()
@@ -14,7 +17,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
+    print config_name
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
@@ -39,3 +42,18 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
+
+def create_celery_app(app=None):
+    app = app or create_app('staging')
+    celery = Celery(__name__, broker='sqla+sqlite:///celerydb.sqlite',
+        backend='db+sqlite:///celerydb.sqlite')
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+
+    return celery
