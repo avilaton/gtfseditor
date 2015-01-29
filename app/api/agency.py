91b124bc284ac4528b0e0ad45fd461c070e6641f
Gaston Avila <avila.gas@gmail.com>
2015-01-28 20:18:47 -0800

    cleanup

--- app/api/agency.py
+++ app/api/agency.py
@@ -12,18 +12,31 @@ from app.tasks import buildFeed
 @api.route('/agency')
 @api.route('/agency/')
 def get_agencys():
-    sendEmail.delay("test")
-    print "starting build"
-    buildFeed.delay()
     agencys = Agency.query.all()
     return jsonify({
         'agencys': [item.to_json for item in agencys]
     })
 
+
+@api.route('/mail')
+@api.route('/mail/')
+def get_agencys():
+    job = sendEmail.delay("test")
+    return jsonify({"task": job.id})
+
+
+@api.route('/feed')
+@api.route('/feed/')
+def get_agencys():
+    job = buildFeed.delay()
+    return jsonify({'task': job.id})
+
+
 @api.route('/agency/<agency_id>')
 def getAgency(agency_id):
     item = Agency.query.get_or_404(agency_id)
     return jsonify(item.to_json)
+
 
 @api.route('/agency/<agency_id>', methods=['PUT'])
 def updateAgency(agency_id):
@@ -34,13 +47,13 @@ def updateAgency(agency_id):
     return jsonify(item.to_json)
 
 
-
 @api.route('/agency/', methods=['OPTIONS', 'POST'])
 def createAgency():
     item = Agency(**request.json)
     db.session.add(item)
     db.session.commit()
     return jsonify(item.to_json), 201,{'Location': url_for('api.getAgency', agency_id=item.agency_id, _external=True)}
+
 
 @api.route('/agency/<agency_id>', methods=['DELETE'])
 def delete_agency(agency_id):
