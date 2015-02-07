c0ab0fa5985521a34aa2ad43ad4ce097e498ca04
Gaston Avila <avila.gas@gmail.com>
2015-02-07 00:19:22 -0800

    remove hardcoded email from config

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ class Config:
     DEBUG=False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USE_SSL = True 
+    MAIL_USE_SSL = True
     MAIL_USE_TLS = False
-    MAIL_USERNAME = 'gtfseditor@gmail.com'
-    MAIL_PASSWORD = 'gtfseditor1234'
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Prueba]'
-    FLASKY_MAIL_SENDER = 'Prueba Gtfseditor <gtfseditor@gmail.com>'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[gtfseditor]'
+    FLASKY_MAIL_SENDER = 'Gtfseditor <gtfseditor@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_SLOW_DB_QUERY_TIME=0.5
     BROKER_URL = os.environ.get('CLOUDAMQP_URL') or 'sqla+sqlite:///celerydb.sqlite'
