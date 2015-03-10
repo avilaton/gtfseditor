b556819538f5bafa2d0eb5eb66014571bbb22ae4
Gaston Avila <avila.gas@gmail.com>
2015-03-10 14:50:54 -0300

    export to csv as manager command
    Conflicts:
    	manage.py

--- manage.py
+++ manage.py
@@ -27,6 +27,44 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 
+def readCsv(Model, filename, mode=None):
+  print("Importing " + Model.__tablename__ + " from : " + filename)
+  with open(filename, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    if not mode:
+      keyMap = {field:field for field in reader.fieldnames}
+      for row in reader:
+        d = {v:codecs.decode(row[k],'utf8') for k, v in keyMap.items()}
+        model = Model(**d)
+        db.session.merge(model)
+      db.session.commit()
+    elif mode in ['direct']:
+      db.engine.execute(Model.__table__.delete())
+      insertStmt = Model.__table__.insert()
+      db.engine.execute(insertStmt, [row for row in reader])
+
+class DictUnicodeProxy(object):
+  def __init__(self, d):
+    self.d = d
+  def __iter__(self):
+    return self.d.__iter__()
+  def get(self, item, default=None):
+    i = self.d.get(item, default)
+    if isinstance(i, unicode):
+      return i.encode('utf-8')
+    return i
+
+def exportToCsv(Model, filename, mode=None):
+  print("Exporting " + Model.__tablename__ + " to : " + filename)
+  with open(filename + Model.__tablename__ + ".csv", 'w') as csvfile:
+    fieldnames = Model.__table__.columns.keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in Model.query.all():
+      writer.writerow(DictUnicodeProxy(row.to_json))
+
+
+
 def extractZip(filename, dest):
   """extract for debuging"""
   if not os.path.exists(dest):
@@ -224,6 +262,56 @@ def activateRoute(route_id=False):
         route.active = False
     db.session.merge(route)
   db.session.commit()
+
+
+@manager.command
+def importCsv(modelname, filename, mode=None):
+  print(filename)
+  Model = globals()[modelname]
+  readCsv(Model, filename, mode=mode)
+
+
+@manager.command
+def importCba(folder):
+  """Usage:
+  FLASK_CONFIG=local ./manage.py importCba <FOLDER>
+  """
+
+  importCsv("Agency", folder + "agency.csv")
+  importCsv("Calendar", folder + 'calendar.csv')
+  importCsv("CalendarDate", folder + 'calendar_dates.csv')
+  importCsv("FareAttribute", folder + 'fare_attributes.csv')
+  importCsv("FareRule", folder + 'fare_rules.csv')
+  importCsv("FeedInfo", folder + 'feed_info.csv')
+  importCsv("RouteFrequency", folder + 'route_frequencies.csv')
+  importCsv("Route", folder + 'routes.csv')
+  importCsv("Stop", folder + 'stops.csv')
+  importCsv("Shape", folder + 'shapes.csv', mode='direct')
+  importCsv("Trip", folder + 'trips.csv')
+  importCsv("TripStartTime", folder + 'trips_start_times.csv')
+  importCsv("StopSeq", folder + 'stop_seq.csv', mode='direct')
+  importCsv("StopTime", folder + 'stop_times.csv', mode='direct')
+  importCsv("Frequency", folder + 'frequencies.csv')
+
+
+@manager.command
+def export(folder=".tmp/export/"):
+
+  exportToCsv(Agency, folder)
+  exportToCsv(Calendar, folder)
+  exportToCsv(CalendarDate, folder)
+  exportToCsv(FareAttribute, folder)
+  exportToCsv(FareRule, folder)
+  exportToCsv(FeedInfo, folder)
+  exportToCsv(RouteFrequency, folder)
+  exportToCsv(Route, folder)
+  exportToCsv(Stop, folder)
+  exportToCsv(Shape, folder)
+  exportToCsv(Trip, folder)
+  exportToCsv(TripStartTime, folder)
+  exportToCsv(StopSeq, folder)
+  exportToCsv(StopTime, folder)
+  exportToCsv(Frequency, folder)
 
 
 if __name__ == '__main__':
