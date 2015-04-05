24def8cae722fbb0a462761227ba802b1f31a2c1
Gaston Avila <avila.gas@gmail.com>
2015-04-04 22:56:54 -0300

    fix shapes endpoints

--- app/api/shapes.py
+++ app/api/shapes.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import logging
-logger = logging.getLogger(__name__)
 
-from flask import jsonify, request, g, abort, url_for, current_app
+from flask import jsonify
+from flask import request
 from .. import db
 from ..models import Shape
 from . import api
 import app.services.geojson as geojson
 
-
+# DEPRECATED
 @api.route('/shape/<shape_id>')
 def getShape(shape_id):
   shape = db.session.query(Shape).filter(Shape.shape_id == shape_id)\
-  	.order_by(Shape.shape_pt_sequence).all()
+    .order_by(Shape.shape_pt_sequence).all()
   coords = [[pt.shape_pt_lon,pt.shape_pt_lat] for pt in shape]
   feature = geojson.feature(id=shape_id, feature_type="LineString",
-  	coords=coords, properties={})
+    coords=coords, properties={})
   return jsonify(geojson.featureCollection([feature]))
 
+# DEPRECATED
 @api.route('/shape/<shape_id>',  methods=['PUT'])
 def updateShape(shape_id):
   geojsonShape = request.json
@@ -37,3 +37,70 @@ def updateShape(shape_id):
         db.session.add(shape_pt)
 
   return jsonify({'success': True,'shape_id': shape_id})
+
+
+@api.route('/shapes/<shape_id>.json')
+def getShapeById(shape_id):
+  shape = db.session.query(Shape).filter_by(shape_id=shape_id)\
+  	.order_by(Shape.shape_pt_sequence).all()
+
+  return jsonify({
+    "shape_id": shape_id,
+    "coordinates": [[pt.shape_pt_lon, pt.shape_pt_lat] for pt in shape]
+    })
+
+
+@api.route('/shapes', methods=['POST'])
+@api.route('/shapes/', methods=['POST'])
+def createShape():
+  data = request.json
+  coordinates = data.get('coordinates', [])
+  shape_id = db.session.query(db.func.max(Shape.shape_id)).scalar() + 1
+  for i, pt in enumerate(coordinates):
+    d = {
+      'shape_id': shape_id,
+      'shape_pt_lon': pt[0],
+      'shape_pt_lat': pt[1],
+      'shape_pt_sequence': i+1
+      }
+    shape_pt = Shape(**d)
+    db.session.add(shape_pt)
+  db.session.commit()
+
+  return jsonify({
+    'shape_id': shape_id,
+    'coordinates': coordinates
+    })
+
+
+@api.route('/shapes/<shape_id>.json', methods=['PUT'])
+def updateShapeById(shape_id):
+  data = request.json
+  coordinates = data.get('coordinates', [])
+  db.session.query(Shape).filter_by(shape_id=shape_id).delete()
+
+  for i, pt in enumerate(coordinates):
+    d = {
+      'shape_id': shape_id,
+      'shape_pt_lon': pt[0],
+      'shape_pt_lat': pt[1],
+      'shape_pt_sequence': i+1
+      }
+    shape_pt = Shape(**d)
+    db.session.add(shape_pt)
+  db.session.commit()
+
+  return jsonify({
+    'shape_id': shape_id,
+    'coordinates': coordinates
+    })
+
+
+@api.route('/shapes/<shape_id>.json', methods=['DELETE'])
+def deleteShapeById(shape_id):
+  db.session.query(Shape).filter_by(shape_id=shape_id).delete()
+  db.session.commit()
+
+  return jsonify({
+    'shape_id': shape_id
+    })
