612b69aeb5fd4849c2e7638c804b274decd2e144
Gaston Avila <avila.gas@gmail.com>
2015-04-30 18:07:22 -0300

    add user roles to models

---
+++ app/api/decorators.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from flask import g
+from .errors import forbidden
+from .models import Permission
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.current_user.can(permission):
+                return forbidden('Insufficient permissions')
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+def admin_required(f):
+    return permission_required(Permission.ADMINISTER)(f)
