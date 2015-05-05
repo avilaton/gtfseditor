49ea2d856549f5685c3670cafc610deb6790c6a3
Gaston Avila <avila.gas@gmail.com>
2015-05-05 16:10:11 -0300

    define shape_id

--- app/api/shapes.py
+++ app/api/shapes.py
@@ -95,7 +95,7 @@ def getTripShape(trip_id):
     abort(404, 'shape not found')
 
   return jsonify({
-    "shape_id": shape_id,
+    "shape_id": shape[0].shape_id,
     "coordinates": [[pt.shape_pt_lon, pt.shape_pt_lat] for pt in shape]
     })
 
@@ -126,17 +126,13 @@ def createTripShape(trip_id):
 @api.route('/trips/<trip_id>/shape.json', methods=['PUT'])
 @admin_required
 def updateTripShape(trip_id):
+  shape_id = db.session.query(Trip.shape_id).filter_by(trip_id=trip_id).scalar()
   coordinates = request.json.get('coordinates', [])
   db.session.query(Shape).filter_by(shape_id=shape_id).delete()
 
   for i, pt in enumerate(coordinates):
-    d = {
-      'shape_id': shape_id,
-      'shape_pt_lon': pt[0],
-      'shape_pt_lat': pt[1],
-      'shape_pt_sequence': i+1
-      }
-    shape_pt = Shape(**d)
+    shape_pt = Shape(shape_id=shape_id, shape_pt_lon=p[0], shape_pt_lat=p[1],
+      shape_pt_sequence=i+1)
     db.session.add(shape_pt)
   db.session.commit()
 
