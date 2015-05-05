045d5e3623edb67f74084e344dc2aa6ce2e9807b
Gaston Avila <avila.gas@gmail.com>
2015-05-05 08:59:35 -0300

    hotfix config error

--- config.py
+++ config.py
@@ -100,7 +100,7 @@ class ProductionConfig(Config):
         mail_handler = SMTPHandler(
             mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
             fromaddr=cls.FLASKY_MAIL_SENDER,
-            toaddrs=[cls.FLASKY_ADMIN],
+            toaddrs=[cls.ADMIN_EMAIL],
             subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
             credentials=credentials,
             secure=secure)
