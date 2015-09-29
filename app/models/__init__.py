7118ce326f8a4d5ea630c95ef477bfbf2dd97da5
Gaston Avila <avila.gas@gmail.com>
2015-09-28 23:28:36 -0300

    remove unused models file

---
+++ app/models/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from app.models.agency import Agency
+from app.models.calendar import Calendar
+from app.models.calendardate import CalendarDate
+from app.models.fareattribute import FareAttribute
+from app.models.farerule import FareRule
+from app.models.feedinfo import FeedInfo
+from app.models.frequency import Frequency
+from app.models.permission import Permission
+from app.models.role import Role
+from app.models.route import Route
+from app.models.routefrequency import RouteFrequency
+from app.models.shape import Shape
+from app.models.shapepath import ShapePath
+from app.models.stop import Stop
+from app.models.stopseq import StopSeq
+from app.models.stoptime import StopTime
+from app.models.transfer import Transfer
+from app.models.trip import Trip
+from app.models.tripstarttime import TripStartTime
+from app.models.user import User
