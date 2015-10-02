ba9ab777c389babd51308a6edf56e93b9de3017b
Gaston Avila <avila.gas@gmail.com>
2015-10-02 18:48:50 -0300

    fix creation of new service period

--- app/commands/deploy.py
+++ app/commands/deploy.py
@@ -1,21 +1,25 @@
+import logging
 
 from flask.ext.migrate import upgrade
 from flask.ext.script import Command, Option
 
 from app.models import Role
 
+logger = logging.getLogger(__name__)
 
 class Deploy(Command):
     """Run deployment tasks."""
 
     def get_options(self):
         return [
-            Option('-a', '--addroles', dest='addroles', action="store_true", default=False),
+            Option('-a', '--addroles', dest='addroles', action="store_true", default=True),
         ]
 
-    def run(self, addroles=False):
+    def run(self, addroles=True):
         # migrate database to latest revision
+        logger.info('Upgrading DB')
         upgrade()
 
         if addroles:
+            logger.info('Inserting Roles into DB')
             Role.insert_roles()
