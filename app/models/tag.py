4714e95b43be66e0ddd4752dc28d8d4438882c50
Gaston Avila <avila.gas@gmail.com>
2015-10-30 11:34:37 -0300

    fix agency tags relationships

--- app/models/tag.py
+++ app/models/tag.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, types, ForeignKey
+from sqlalchemy import Column, types, ForeignKeyConstraint, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy_continuum import version_class
 
 from .base import Base
+from .gtfs import Agency
+
+AgencyVersion = version_class(Agency)
 
 
 class Tag(Base):
@@ -12,14 +16,17 @@ class Tag(Base):
 
     id = Column(types.Integer, primary_key=True)
     name = Column(types.String(50))
-    agencies = relationship("AgencyTag", backref="tag")
+    agency_versions = relationship("AgencyVersion", secondary="agency_tags", backref="tags")
 
 
 class AgencyTag(Base):
-    __tablename__ = 'agency_tag'
+    __tablename__ = 'agency_tags'
 
-    id = Column(types.Integer, primary_key=True)
-    agency_id = Column(types.Integer, ForeignKey('agency_version.agency_id'), primary_key=True)
-    transaction_id = Column(types.Integer, ForeignKey('agency_version.transaction_id'), primary_key=True)
-    tag_id = Column(types.Integer, ForeignKey('tags.id'))
+    agency_id = Column(types.Integer, primary_key=True)
+    transaction_id = Column(types.Integer)
+    tag_id = Column(types.Integer, ForeignKey('tags.id'), primary_key=True)
+
+    __table_args__ = (ForeignKeyConstraint([agency_id, transaction_id],
+                                           [AgencyVersion.agency_id, AgencyVersion.transaction_id]),
+                      {})
 
