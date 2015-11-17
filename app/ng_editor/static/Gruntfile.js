d7c8ab9f9d1f583b7d6ccfffa95cd262345b65dd
Gaston Avila <avila.gas@gmail.com>
2015-11-17 09:32:03 -0300

    add ol3 dependency

--- app/ng_editor/static/Gruntfile.js
+++ app/ng_editor/static/Gruntfile.js
@@ -86,6 +86,10 @@ module.exports = function (grunt) {
                 connect.static('./bower_components')
               ),
               connect().use(
+                '/node_modules',
+                connect.static('node_modules')
+              ),
+              connect().use(
                 '/app/styles',
                 connect.static('./app/styles')
               ),
