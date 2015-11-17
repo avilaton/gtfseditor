29e58ce76081fa9a97cac6559dccb014275f40cc
Gaston Avila <avila.gas@gmail.com>
2015-11-17 10:00:24 -0300

    wip trip maps

--- app/ng_editor/static/app/scripts/controllers/trip.js
+++ app/ng_editor/static/app/scripts/controllers/trip.js
@@ -40,4 +40,15 @@ angular.module('editorApp')
     $scope.cancel = function () {
       $window.history.back();
     };
+
+    new ol.Map({
+      layers: [
+        new ol.layer.Tile({source: new ol.source.OSM()})
+      ],
+      view: new ol.View({
+        center: [0, 0],
+        zoom: 2
+      }),
+      target: 'map'
+    });
   });
