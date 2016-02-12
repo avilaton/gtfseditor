c4e4522c57d406cac81f5919f63a9a7bcb87d1d5
Gaston Avila <avila.gas@gmail.com>
2016-02-12 13:18:42 -0300

    WIP agency views

--- app/editor/views.py
+++ app/editor/views.py
@@ -3,6 +3,7 @@ from flask import (send_from_directory,
                    request,
                    flash,
                    redirect, url_for)
+from flask.views import MethodView
 from flask.ext.login import login_required
 
 from app.editor import editor
@@ -17,43 +18,70 @@ from app.utils import flash_errors
 def root():
     return send_from_directory(editor.static_folder, 'index.html')
 
-@editor.route('/agencies')
-@login_required
-def list_agencies():
-    agencies = Agency.query.order_by(Agency.agency_name).all()
-    return render_template('agencies/list.html', agencies=agencies)
 
-@editor.route('/agencies/new', methods=['GET', 'POST'])
-@editor.route('/agencies/<agency_id>', methods=['GET', 'POST'])
-@login_required
-def create_or_edit_agency(agency_id=None):
-    if agency_id:
+class AgenciesView(MethodView):
+
+    @login_required
+    def get(self):
+        agencies = Agency.query.order_by(Agency.agency_name)
+        return render_template('agencies/list.html', agencies=agencies)
+
+
+class AgencyView(MethodView):
+
+    @login_required
+    def get(self, agency_id):
         agency = Agency.query.get(agency_id)
-    else:
-        agency = None
-    form = AgencyForm(request.form, agency, csrf_enabled=False)
+        return render_template('agencies/detail.html', agency=agency)
 
-    if form.validate_on_submit():
-        if not agency:
+
+class AgencyEditView(MethodView):
+
+    @login_required
+    def get(self, agency_id):
+        if agency_id:
+            agency = Agency.query.get(agency_id)
+        else:
+            agency = None
+        form = AgencyForm(request.form, agency, csrf_enabled=False)
+        return render_template('agencies/form.html', form=form, agency=agency)
+
+    @login_required
+    def post(self, agency_id):
+        if agency_id:
+            agency = Agency.query.get(agency_id)
+        else:
             agency = Agency()
             db.session.add(agency)
-            flash('Agency created.', 'success')
-        else:
+        form = AgencyForm(request.form, agency, csrf_enabled=False)
+
+        if form.validate_on_submit():
+            form.populate_obj(agency)
+            db.session.commit()
             flash('Agency updated.', 'success')
-        form.populate_obj(agency)
-        db.session.commit()
-        return redirect(url_for('editor.list_agencies'))
+            return redirect(url_for('editor.agencies'))
 
-    return render_template('agencies/form.html', form=form, agency=agency)
+        return render_template('agencies/form.html', form=form, agency=agency)
 
-@editor.route('/agencies/<int:agency_id>/delete')
-@login_required
-def delete_agency(agency_id):
-    agency = Agency.query.get(agency_id)
-    if agency:
-        db.session.delete(agency)
-        db.session.commit()
-        flash('Agency deleted.', 'success')
-        return redirect(url_for('editor.list_agencies'))
-    else:
-        return redirect(url_for('editor.list_agencies'))
+    @login_required
+    def delete(self, agency_id):
+        agency = Agency.query.get(agency_id)
+        if agency:
+            db.session.delete(agency)
+            db.session.commit()
+            flash('Agency deleted.', 'success')
+            return redirect(url_for('editor.agencies'))
+        else:
+            return redirect(url_for('editor.agencies'))
+
+
+
+agencies_view = AgenciesView.as_view('agencies')
+editor.add_url_rule('/agencies', view_func=agencies_view)
+
+agency_view = AgencyView.as_view('agency')
+editor.add_url_rule('/agencies/<agency_id>', view_func=agency_view)
+
+agency_edit_view = AgencyEditView.as_view('agency_edit')
+editor.add_url_rule('/agencies/<agency_id>/edit', view_func=agency_edit_view)
+editor.add_url_rule('/agencies/new', defaults={'agency_id': None}, view_func=agency_edit_view)
