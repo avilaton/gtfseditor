c60443baf6ae33fe53da558ffb904cd82ec629fd
Gaston Avila <avila.gas@gmail.com>
2016-02-18 23:28:29 -0300

    Add routeTrip view

--- app/editor/static/app/scripts/router.js
+++ app/editor/static/app/scripts/router.js
@@ -4,15 +4,17 @@ define([
   'backbone',
   'views/stops',
   'views/routes',
+  'views/routeTrip',
   'views/calendar',
   'views/agencies',
   'views/navbarRight'
-], function ($, _, Backbone, StopsView, RoutesView, CalendarView, AgenciesView,
+], function ($, _, Backbone, StopsView, RoutesView, RouteTripView, CalendarView, AgenciesView,
   NavbarRightView){
 
   var AppRouter = Backbone.Router.extend({
     routes: {
       'routes(/:route_id)': 'showRoute',
+      'routes/:route_id/trips/:trip_id': 'showTrip',
       'stops(/:stop_id)': 'stopsView',
       'calendar(/)': 'calendarView',
       'agencies(/:agency_id)': 'agenciesView',
@@ -34,6 +36,13 @@ define([
     app_router.on('route:showRoute', function(route_id){
       clean();
       mainView = new RoutesView();
+    });
+    app_router.on('route:showTrip', function(route_id, trip_id){
+      clean();
+      mainView = new RouteTripView({
+        route_id: route_id,
+        trip_id: trip_id
+      });
     });
     app_router.on('route:stopsView', function(route_id){
       clean();
