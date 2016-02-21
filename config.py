a8210355dade09d3161cca2633e6ecb2d1a2af40
Gaston Avila <avila.gas@gmail.com>
2016-02-21 19:03:08 -0300

    Add config DEBUG option

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    DEBUG = os.environ.get('SECRET_KEY') or False
+    DEBUG = bool(os.environ.get('DEBUG') in ['yes', 1, 'true', 'True'])
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SSL_DISABLE = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
