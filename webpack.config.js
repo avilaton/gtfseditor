4e965f836734298ca047a52b41316ad4d6fda40b
Gaston Avila <avila.gas@gmail.com>
2017-08-31 01:18:23 -0300

    Update webpack and ol

--- webpack.config.js
+++ webpack.config.js
@@ -8,34 +8,37 @@ var CleanWebpackPlugin = require('clean-webpack-plugin');
 var rootAssetPath = './app/assets';
 
 config = {
+    context: path.resolve(__dirname, './app/assets'),
     entry: {
         app_js: [
-            rootAssetPath + '/scripts/index'
+            'scripts/index'
         ],
         app_css: [
-            rootAssetPath + '/styles/main.scss'
+            'styles/main.scss'
         ]
     },
     output: {
-        path: './app/static/dist',
+        path: path.resolve(__dirname, './app/static/dist'),
         publicPath: 'http://localhost:2992/assets/',
         filename: '[name].[chunkhash].js',
         chunkFilename: '[id].[chunkhash].js'
     },
     resolve: {
-        extensions: ['', '.js', '.css', '.scss'],
-        modulesDirectories: [
+        extensions: ['.js', '.css', '.scss'],
+        modules: [
+            path.resolve(__dirname, './app/assets/'),
             'node_modules'
         ],
-        alias: {
-            openlayers3: "openlayers/dist/ol.js",
-        },
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.js$/i, loader: 'ng-annotate!babel?presets[]=es2015',
-                exclude: /node_modules/,
+                test: /\.js$/i,
+                use: [
+                  'ng-annotate-loader',
+                  'babel-loader'
+                ],
+                exclude: [/node_modules/],
             },
             // {
             //     test: /\.(jpe?g|png|gif|svg([\?]?.*))$/i,
@@ -46,29 +49,31 @@ config = {
             // },
             {
                 test: /\.html$/,
-                loaders: ['ngtemplate?relativeTo=' + path.join(__dirname, rootAssetPath), 'raw', 'html-minify'],
-                exclude: /(node_modules)/
+                use: ['ngtemplate-loader?relativeTo=' + path.join(__dirname, rootAssetPath), 'raw-loader', 'html-minify-loader'],
+                exclude: [/(node_modules)/]
             },
             {
                 test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
-                loader: "file"
+                use: ["file-loader"]
             },
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style', 'css!sass')
+                use: ExtractTextPlugin.extract({
+                  use: [
+                    'css-loader', 'sass-loader'
+                  ]
+                })
             }
         ],
-        noParse: [/dist\/ol.js/],
     },
     plugins: [
-        new ExtractTextPlugin('[name].[chunkhash].css'),
+        new ExtractTextPlugin({
+          filename:'[name].[chunkhash].css'
+        }),
         new ManifestRevisionPlugin(path.join('app/static', 'manifest.json'), {
             rootAssetPath: rootAssetPath,
             ignorePaths: ['/styles', '/scripts']
         }),
-        new webpack.ProvidePlugin({
-            ol: "openlayers3",
-        })
     ]
 
 };
